777666c749c56c6df18e49d74960ab8ce2658a5d
Raidez <alexis.lucien@outlook.fr>
2022-01-19 12:33:17 +0100

    - ajout de la corde (physique)

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -2,12 +2,16 @@ extends Camera2D
 
 export (NodePath) var player_path
 export (NodePath) var ship_path
-onready var player = get_node(player_path)
-onready var ship = get_node(ship_path)
 
 const max_distance = 572
 
 func _process(delta):
+	if not player_path or not ship_path:
+		return
+	
+	var player = get_node(player_path)
+	var ship = get_node(ship_path)
+	
 	# détermine la position de la caméra entre le bateau et le joueur
 	var a = player.get_node("CameraPivot").global_position
 	var b = ship.get_node("CameraPivot").global_position
